6d68d31c96eba1bdc8fd72988b1de0612bdff134
amite <amit.erandole@gmail.com>
2017-07-07 19:48:39 +0530

    added code samples

--- presentation/index.js
+++ presentation/index.js
@@ -35,7 +35,8 @@ const theme = createTheme({
   secondary: "white",
   tertiary: "#f7df1e",
   quartenary: "#CECECE"
-}, {
+  },
+  {
     primary: "Montserrat",
     secondary: "Helvetica"
   });
@@ -71,9 +72,29 @@ export default class Presentation extends React.Component {
         <Slide transition={["spin", "slide"]} bgColor="primary">
           <Heading margin="30" size={6} caps fit textColor="tertiary">Let's get it started 🎉</Heading>
           <Terminal title="1. amite: ~(zsh)" output={[
-            "npm install react react-dom next",
+            "npm install babel-core babel-loader babel-preset-env webpack --save-dev",
 
           ]}
+          />
+        </Slide>
+        <Slide bgImage="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=08ccf46749538ff73543865840c47fca" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Esnext support with babel
+          </Heading>
+          <CodePane
+            textSize={16}
+            lang="js"
+            source={require("raw-loader!../assets/code/es-webpack-with-babel.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            You might need custom .babelrc
+          </Heading>
+          <CodePane
+            textSize={15}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-custom-babelrc.example")}
           />
         </Slide>
         <Slide bgImage="https://images.unsplash.com/photo-1490041125797-76d80076bf80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=36b47feeaec8b55cf01be5cd46cbae1a" bgDarken={0.50} transition={["zoom"]} bgColor="primary">
@@ -84,6 +105,55 @@ export default class Presentation extends React.Component {
             What they are and how to use them!
           </Text>
         </Slide>
+        <Slide bgImage="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=08ccf46749538ff73543865840c47fca" bgDarken={0.60} transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Using the common for loop - Exercise
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} bold>
+            http://bit.ly/es5-for-loop
+          </Text>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Using the common for loop - Solution
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es5-for-loop.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Using higher order function MAP - Solution
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es-6-hoc-map.example")}
+          />
+        </Slide>
+        <Slide bgImage="https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=08ccf46749538ff73543865840c47fca" bgDarken={0.60} transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Getting lost in `this` world
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es5-this-bug.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Did anybody order a NaN?
+          </Heading>
+          <CodePane
+            textSize={15}
+            lang="js"
+            source={require("raw-loader!../assets/code/es5-this-nan.example")}
+          />
+        </Slide>
+
         <Slide bgImage="https://images.unsplash.com/photo-1497146135827-2b04f80ccd23?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=c22825dd54f9842fa704a7850dba6035" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Module Two - Defaults and Destructuring
@@ -91,6 +161,91 @@ export default class Presentation extends React.Component {
           <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
             Remove  guard clauses and repetition from your code
           </Text>
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Too much to guard against
+          </Heading>
+          <CodePane
+            textSize={15}
+            lang="js"
+            source={require("raw-loader!../assets/code/es5-no-defaults.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            No more Guards 😀
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-defaults")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Destructure complex objects
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-destructure-profile.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Destructure complex objects - Solution
+          </Heading>
+          <CodePane
+            textSize={15}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-destructure-profile-solution.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Destructure arrays
+          </Heading>
+          <CodePane
+            textSize={15}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-destructure-arrays.example")}
+          />
+        </Slide>
+
+        <Slide bgImage="https://images.unsplash.com/photo-1477003009257-97a7b7c598e0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=097b611bf1eb55536063bed83245f1ee" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Module Six - Using Object and Array Spread
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            Learn to spreee..aaaad that data
+          </Text>
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Adding arrays using the array spread operator
+          </Heading>
+          <CodePane
+            textSize={15}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-spreading-concat-arrays.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Object Spread Operator
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-object-spread.example")}
+          />
         </Slide>
         <Slide bgImage="https://images.unsplash.com/photo-1495210960925-18e290e30641?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=b5ea36f4e82cd5ecc591540adfe9e018" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
@@ -100,6 +255,56 @@ export default class Presentation extends React.Component {
             How and when to use es6 classes?
           </Text>
         </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Do classes really exist in Javascript?
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-class-basics.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Getters and Setters work 😃
+          </Heading>
+          <CodePane
+            textSize={20}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-getters-setters.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            You can have static properties
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-static-properties.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Use classes to generate objects
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-class-as-factory.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Inheritance without explicit prototypes
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-class-inheritance.example")}
+          />
+        </Slide>
         <Slide bgImage="https://images.unsplash.com/photo-1493217465235-252dd9c0d632?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=30fa16d53354bae230f354209e64105b" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Module Four - Use modules to organize your application
@@ -108,6 +313,16 @@ export default class Presentation extends React.Component {
             Organize, Design and Import/Export your code
           </Text>
         </Slide>
+
+        <Slide bgColor="primary">
+          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
+            Different ways to export your code
+          </Heading>
+          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
+            http://bit.ly/es6-export-syntax
+          </Text>
+        </Slide>
+
         <Slide bgImage="https://images.unsplash.com/photo-1473655551229-a39d1a982885?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=dc9f45cc5872fe040f366ece9ffdceca" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Module Five - The joy of es6 template strings
@@ -116,14 +331,29 @@ export default class Presentation extends React.Component {
             Say goodbye to template engines
           </Text>
         </Slide>
-        <Slide bgImage="https://images.unsplash.com/photo-1477003009257-97a7b7c598e0?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=097b611bf1eb55536063bed83245f1ee" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Module Six - Using Object and Array Spread
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Simple template rendering
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            Learn to spreee..aaaad that data
-          </Text>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-simple-template.example")}
+          />
         </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Looping data inside a multiline template
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-template-string-function.example")}
+          />
+        </Slide>
+
         <Slide bgImage="https://images.unsplash.com/photo-1486229868416-95ac9da0a349?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=4068f278db83d372a48dd3dbd23de7b6" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
           <Heading size={1} fit caps lineHeight={1} textColor="secondary">
             Module Seven - Block scoping with let and const
@@ -140,14 +370,148 @@ export default class Presentation extends React.Component {
             The asynchronous side of Javascript
           </Text>
         </Slide>
-        <Slide bgImage="https://images.unsplash.com/photo-1492464063426-31c70befa4b7?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1600&h=900&fit=crop&s=e02d5aa11564f5ad309f806689112d32" bgDarken={0.40} transition={["zoom"]} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Module Nine - Map and Sets
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            The trouble with callbacks
           </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            New Data Structures in Javascript
-          </Text>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-callbacks.example")}
+          />
         </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            What are callbacks?
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-event-callback.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Are all callbacks asynchronous?
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-synchronous-callbacks.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Asynchronous callbacks
+          </Heading>
+          <CodePane
+            textSize={17}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-ajax-callback.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={1} lineHeight={1} textColor="secondary">
+            Getting Promises at a coffee shop ☕️
+          </Heading>
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={5} lineHeight={1} textColor="tertiary">
+            Promises can be in three states
+          </Heading>
+          <Appear>
+            <List>
+              <ListItem>Pending</ListItem>
+              <ListItem>Fulfilled</ListItem>
+              <ListItem>Rejected</ListItem>           
+            </List>
+          </Appear>
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Writing your own Promise - I
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-writing-own-promise.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Writing your own Promise - II
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-writing-own-promise-2.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            From callbacks to promises
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-callbacks-to-promises.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Promises can be resolved together
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-promises-all.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Here Comes Async
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-simple-async.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Making Concurrent Requests
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-concurrent-async-requests.example")}
+          />
+        </Slide>
+
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="secondary">
+            Making Parallel Requests with Promise.all
+          </Heading>
+          <CodePane
+            textSize={19}
+            lang="js"
+            source={require("raw-loader!../assets/code/es6-parallel-async-requests.example")}
+          />
+        </Slide>
+        <Slide transition={["zoom"]} bgColor="primary">
+          <Heading size={6} lineHeight={1} textColor="tertiary">
+            Async Coding Challenge
+          </Heading>
+          <Text size={6} textColor="secondary">http://bit.ly/chained-promises</Text>
+        </Slide>
+
         <Slide transition={["fade"]} bgColor="tertiary">
           <Heading size={6} textColor="primary" caps>Typography</Heading>
           <Heading size={1} textColor="secondary">Heading 1</Heading>
